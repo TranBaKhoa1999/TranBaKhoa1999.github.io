d404abc6b9352c9fea50cc1c9d3d38ca75f42781
Khoa <ohwhynotme1999@gmail.com>
2020-08-09 21:31:40 +0700

    fix navbar ( trunk nav)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,3 +2,41 @@ window.addEventListener("scroll",function(){
     var header= document.getElementById("header");
     header.classList.toggle("sticky",window.scrollY>200);
 });
+//animation and event menu button
+const menuBtn = document.querySelector('.checkbtn');
+const content =document.querySelector('#page');
+var header= document.getElementById("header");
+var nav = document.querySelector('.navbar-nav');
+let menuOpen = false;
+menuBtn.addEventListener('click',()=>{
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen=true;
+        content.style.width='50%';
+        header.style.width='50%';
+        nav.style.right='0';
+    }
+    else{
+        menuBtn.classList.remove('open');
+        menuOpen=false;
+        content.style.width='100%';
+        header.style.width='100%';
+        nav.style.right='-100%';
+    }
+});
+//
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+    } 
+    else {
+        menuOpen=false;
+        nav.style.right='-100%';
+        menuBtn.classList.remove('open');
+        content.style.width='100%';
+        header.style.width='100%';
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 805px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
