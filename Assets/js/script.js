59648a5df9880c9476ca06ca8628f1220ce9728c
Khoa <ohwhynotme1999@gmail.com>
2020-08-13 00:43:28 +0700

    add slide intro

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,49 +1,47 @@
-window.addEventListener("scroll",function(){
-    var header= document.getElementById("header");
-    header.classList.toggle("sticky",window.scrollY>100);
+window.addEventListener("scroll", function () {
+    var header = document.getElementById("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
 });
 //animation and event menu button
 var menuBtn = document.querySelector('.checkbtn');
-const content =document.querySelector('#page');
-var header= document.getElementById("header");
+const content = document.querySelector('#page');
+var header = document.getElementById("header");
 var nav = document.querySelector('.navbar-nav');
 let menuOpen = false;
-menuBtn.addEventListener('click',()=>{
-    if(!menuOpen){
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
         menuBtn.classList.add('open');
-        menuOpen=true;
+        menuOpen = true;
         // content.style.width='50%';
         // header.style.width='50%';
-        nav.style.right='0';
-        header.style.left='-50%';
-        content.style.left='-50%';
-    }
-    else{
+        nav.style.right = '0';
+        header.style.left = '-50%';
+        content.style.left = '-50%';
+    } else {
         menuBtn.classList.remove('open');
-        menuOpen=false;
+        menuOpen = false;
         // content.style.width='100%';
         // header.style.width='100%';
-        header.style.left='0';
-        content.style.left='0';
-        nav.style.right='-100%';
+        header.style.left = '0';
+        content.style.left = '0';
+        nav.style.right = '-100%';
     }
 });
 //
 function myFunction(x) {
     if (x.matches) { // If media query matches
-    } 
-    else {
-        menuOpen=false;
-        nav.style.right='-100%';
+    } else {
+        menuOpen = false;
+        nav.style.right = '-100%';
         menuBtn.classList.remove('open');
         // content.style.width='100%';
         // header.style.width='100%';
-        header.style.left='0';
-        content.style.left='0';
+        header.style.left = '0';
+        content.style.left = '0';
     }
-  }
-  
-  var x = window.matchMedia("(max-width: 805px)")
-  myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes
-  // jquery
+}
+
+var x = window.matchMedia("(max-width: 805px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+// jquery
--- Assets/js/scroll_image.js
+++ Assets/js/scroll_image.js
@@ -1,6 +1,9 @@
-$(window).scroll(function(){
-    var scroll_value = $(window).scrollTop();
-    $('#intro').css({
-      'background-position-y':- scroll_value +'px'
+$(window).scroll(function () {
+  var start_Resume = 600;
+  if (window.scrollY > start_Resume) {
+    var scroll_value = $(window).scrollTop() - start_Resume;
+    $('#resume').css({
+      'background-position-y': -scroll_value + 'px'
     });
+  }
 });
